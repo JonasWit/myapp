e581d41f541f83f2fab31d472ab17e938390b6d5
JW <jonasz.witek@pm.me>
2024-12-08 19:26:58 +0100

    combobox

--- src/dotnet/DotnetService/DotnetService/ViewModels/HomePageViewModel.cs
+++ src/dotnet/DotnetService/DotnetService/ViewModels/HomePageViewModel.cs
@@ -1,4 +1,6 @@
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using DotnetService.ServiceLoaders;
 
 namespace DotnetService.ViewModels;
@@ -6,5 +8,18 @@ namespace DotnetService.ViewModels;
 [ApplicationSingletonViewModel]
 public partial class HomePageViewModel : ViewModelBase
 {
+    [ObservableProperty] private ObservableCollection<string> _comboboxItems;
+
+    [ObservableProperty] private string _comboboxSelectedItem = "";
     [ObservableProperty] private string _pageTitle = "Home";
+
+    public HomePageViewModel()
+    {
+        ComboboxItems = ["Home", "Not Home"];
+    }
+
+    [RelayCommand]
+    private void ComboboxSelectionChanged()
+    {
+    }
 }
